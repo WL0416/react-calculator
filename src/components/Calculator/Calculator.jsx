75ebbaa831d258e5476a53ff60cc25c6f481d84d
Li Wei <liwsuper7@gmail.com>
2019-03-05 23:17:13 +1100

    change number order

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -7,7 +7,7 @@ class Calculator extends Component {
 
     state = {
         displayValue: '0',
-        numbers: ['9','8','7','6','5','4','3','2','1','.','0','AC'],
+        numbers: ['7','8','9','4','5','6','1','2','3','.','0','AC'],
         operators: ['/','X','-','+'],
         selectedOperator: '',
         storedValue: '',
